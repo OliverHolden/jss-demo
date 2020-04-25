d50973f94b574913e4794ea2e2b662687f280359
Oliver Holden <oliver.holden3@gmail.com>
2020-04-25 20:23:58 +0100

    initial name space issue commit

---
+++ src/Components/NewStyledButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './NewStyledButton.scss'
+
+export default function NewStyledButton() {
+    return (
+        <button className={"button-green"}>Click Me</button>
+    );
+}
